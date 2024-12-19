cf80f3042e99859f6ac0a203bc06cdae2c26ed66
pavlov <danil227pavlov@gmail.com>
2024-12-19 12:17:37 +0300

    Добавление templates

---
+++ templating/blogrender.go
@@ -0,0 +1,72 @@
+package templating
+
+import (
+	"embed"
+	"github.com/gomarkdown/markdown/parser"
+	"html/template"
+	"io"
+	"strings"
+)
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type PostRenderer struct {
+	templ    *template.Template
+	mdParser *parser.Parser
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	ext := parser.CommonExtensions | parser.AutoHeadingIDs
+	p := parser.NewWithExtensions(ext)
+
+	return &PostRenderer{templ: templ, mdParser: p}, nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	indexTemplate := `<ol>{{range .}}<li><a href="/post/{{sanitiseTitle .Title}}">{{.Title}}</a></li>{{end}}</ol>`
+
+	templ, err := template.New("index").Funcs(template.FuncMap{
+		"sanitiseTitle": func(title string) string {
+			return strings.ToLower(strings.Replace(title, " ", "-", -1))
+		},
+	}).Parse(indexTemplate)
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, posts); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	//p.Body = string(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Render(w io.Writer, p Post) error {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return err
+	}
+
+	if err := templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+		return err
+	}
+
+	return nil
+}
